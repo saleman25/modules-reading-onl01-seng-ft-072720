157abbf85edd3d39b2cd6ec12e74d505949f13ec
Samantha Aleman <samantha.aleman25@gmail.com>
2020-08-12 05:41:24 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -3,19 +3,12 @@ require_relative './class_methods_module.rb'
 require_relative './fancy_dance.rb'
 
 
-class Kid 
+class Kid
+  include Dance
+  extend MetaDancing
+  attr_accessor :name
 
-extend FancyDance::ClassMethods
-  include FancyDance::InstanceMethods
+  def initialize(name)
+    @name = name
+  end
 end
-
-attr_accessor :name 
-
-def initialize(name)
-  @name = name
-end 
-  
-  
-  
-  
-end 
